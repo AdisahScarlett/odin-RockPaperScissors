49acf701af8f190e4f49f835aa92e7df233091fe
Adisah Scarlett <adisah520@gmail.com>
2022-09-22 00:44:32 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-let getComputerChoice = () => {
+const getComputerChoice = () => {
   const choices = ["Rock", "Paper", "Scissors"];
-  let numberPicker = Math.floor(Math.random() * choices.length - 1);
-  return choices[numberPicker]
+  const choicePicker = Math.floor(Math.random() * choices.length);
+  return choices[choicePicker];
+}
+
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection.toLowerCase();
+  computerSelection.toLowerCase();
+
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return `You Lose! Paper beats Rock`;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return `You Lose! Scissors beats Paper`; 
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return `You Lose! Rock beats Scissors`;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return `You Win! Paper beats Rock`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return `You Win! Scissors beats Paper`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `You Win! Rock beats Scissors`;
+  } else {
+    return `You are both tied`;
+  }
+
 }
